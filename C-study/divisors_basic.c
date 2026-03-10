b0aaf2198e85fdada381eacb45abb7c836666893
VisualStudy <sparksoldier@outlook.com>
2026-03-10 22:20:16 +0900

    divisors_basic

---
+++ C-study/divisors_basic.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int i;
+	printf("값 입력: ");
+	scanf("%d", &n);
+
+	printf("약수는 ");
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	
+	printf("이다.\n");
+
+	return 0;
+}
