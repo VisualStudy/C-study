7d6f9fb4c8e706c64b03cf6674cc787e3ce4ff24
VisualStudy <sparksoldier@outlook.com>
2026-03-13 16:17:21 +0900

    fibonacci series master

---
+++ C-study/fibonacci_1.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int i;
+	int n;
+
+	a = 1;
+	b = 1;
+	c = a + b;
+	printf("몇까지 반복: ");
+	scanf("%d", &n);
+	printf("%d + %d = %d\n", a, b, c);
+
+	for (i = 1; i <= n; i++)
+	{
+		a = b;
+		b = c;
+		c = a + b;
+		printf("%d + %d = %d\n", a, b, c);
+	}
+
+
+	return 0;
+}
---
+++ C-study/fibonacci_1_recursive.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fibo(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibo(n - 1) + fibo(n - 2);
+	}
+}
+
+int main(void)
+{
+	int a;
+	printf("몇까지 반복: ");
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++)
+	{
+		printf("%d + %d = %d\n", fibo(i), fibo(i + 1), fibo(i + 2));
+	}
+	
+	return 0;
+}
