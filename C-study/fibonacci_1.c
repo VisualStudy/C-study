de7deb9c3e0d7f36e607de80f702a99a17c9b7d8
VisualStudy <sparksoldier@outlook.com>
2026-03-15 20:06:12 +0900

    fibonacci series

--- C-study/fibonacci_1.c
+++ C-study/fibonacci_1.c
@@ -9,18 +9,25 @@ int main(void)
 	int i;
 	int n;
 
+	printf("C언어로 피보나치 수열 구현하기\n");
+	printf("20256121 박지호\n");
+	printf("초기값은 1, 1입니다.\n");
+	printf("-----------------------------------\n");
+
 	a = 1;
 	b = 1;
 	c = a + b;
+
 	printf("몇까지 반복: ");
 	scanf("%d", &n);
-	printf("%d + %d = %d\n", a, b, c);
+	printf("%d + %d = %d\n", a, b, c); 
 
-	for (i = 1; i <= n; i++)
+	for (i = 1; i < n; i++) 
 	{
 		a = b;
 		b = c;
 		c = a + b;
+
 		printf("%d + %d = %d\n", a, b, c);
 	}
 
