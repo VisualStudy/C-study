0b06eb6355bb262bad3629357395f084bdb2e9e9
VisualStudy <sparksoldier@outlook.com>
2026-03-17 01:19:27 +0900

    gcd

--- C-study/gcd.c
+++ C-study/gcd.c
@@ -1,1 +1,31 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+
+	scanf("%d %d", &x, &y);
+
+	while (x != y)
+	{
+		if (x > y)
+		{
+			printf("x: %d, y: %d\n", x, y);
+			x = x - y;
+		}
+		else
+		{
+			printf("x: %d, y: %d\n", x, y);
+			y = y - x;
+		}
+		if (x == y)
+		{
+			break;
+		}
+	}
+
+	printf("최대공약수: %d\n", x);
+
+	return 0;
+}
