50c389c06849eea16ca98a46cb26ad3257835a69
VisualStudy <sparksoldier@outlook.com>
2026-03-17 01:23:24 +0900

    gcd recursive

---
+++ C-study/gcd_recursive.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int gcd(int x, int y)
+{
+	if (x == y)
+	{
+		return x;
+	}
+	else
+	{
+		if (x > y)
+		{
+			return gcd(x - y, y);
+		}
+		else
+		{
+			return gcd(x, y - x);
+		}
+	}
+}
+
+int main(void)
+{
+	int x;
+	int y;
+
+	printf("두 수 입력: ");
+	scanf("%d %d", &x, &y);
+
+	printf("최대공약수: %d\n", gcd(x, y));
+
+	return 0;
+}
