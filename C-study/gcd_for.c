70fbf90285f462d0af9836c3fea5fb1b9831f032
VisualStudy <sparksoldier@outlook.com>
2026-03-22 21:56:12 +0900

    gcd for loop

---
+++ C-study/gcd_for.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    int xx, yy;
+
+    printf("-----------두 수의 최대공약수 구하기------------\n");
+    printf("두 수를 입력하시오: ");
+    scanf("%d %d", &x, &y);
+
+    xx = x;
+    yy = y;
+
+    printf("%d \t\t|  %d \n", x, y);
+
+    for (; x != y; )
+    {
+        if (x > y)
+        {
+            printf("%d (= %d - %d) \t|  %d \n", x - y, x, y, y);
+            x = x - y;
+        }
+        else
+        {
+            printf("%d \t\t|  %d (= %d - %d) \n", x, y - x, y, x);
+            y = y - x;
+        }
+    }
+
+    printf("두 수의 최대공약수는 %d입니다.", x);
+    printf("\n입력한 두 수 %d와 %d를 나누는 수 중에서 가장 큰 값입니다.", xx, yy);
+
+    return 0;
+}
