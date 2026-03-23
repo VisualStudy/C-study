e1e3f2a2186f77c97c8601bb9f9efeed64b94fb2
VisualStudy <sparksoldier@outlook.com>
2026-03-23 21:40:31 +0900

    prime number

---
+++ C-study/print_primes.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int Prime;
+	
+	for (i = 2; i <= 10000; i++)
+	{
+		Prime = 1;
+
+		for (j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				Prime = 0;
+				break;
+			}
+		}
+
+		if (Prime)
+		{
+			printf("%d\n", i);
+		}
+	}
+	
+	return 0;
+}
