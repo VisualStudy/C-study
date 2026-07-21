479c852075319f3566be7ad1e8f4846297e4c4d1
VisualStudy <sparksoldier@outlook.com>
2026-07-21 22:28:38 +0900

    malloc 테스트 메시지 추가
    "Okay Let's GO" 메시지를 추가로 출력하도록 printf 문을 추가했습니다. 기존 메시지 이후에 바로 출력됩니다.

--- C-study/malloc.c
+++ C-study/malloc.c
@@ -3,7 +3,9 @@
 int main(int argc, char* argv[]) 
 {
     printf("Hello, World!\n");
-    printf("This is a test program for malloc.\n"); 
+    printf("This is a test program for malloc.\n");
+    
+    printf("Okay Let's GO");
 
     return 0;
 }
