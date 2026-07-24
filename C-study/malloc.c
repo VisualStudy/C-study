106027ed9a2ebbf099cbc69426f7da4b38c8f7d9
VisualStudy <sparksoldier@outlook.com>
2026-07-24 22:20:04 +0900

    main 함수에 raba 변수 및 출력 코드 추가
    main 함수에 raba 변수를 선언하고, "Hello, World!" 메시지와 조건문을 통한 추가 출력 기능을 구현했습니다.

--- C-study/malloc.c
+++ C-study/malloc.c
@@ -2,10 +2,17 @@
 
 int main(int argc, char* argv[]) 
 {
+    int raba = 0;
+    
     printf("Hello, World!\n");
     printf("This is a test program for malloc.\n");
     
     printf("Okay Let's GO");
+
+    if (raba == 1)
+    {
+        printf("일어날 일이 없는 일");
+    }
 
     return 0;
 }
